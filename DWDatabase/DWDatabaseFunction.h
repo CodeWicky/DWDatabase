86276618a1f54ff94fc5d0712847045e0b3ac164
Wicky <codewicky@163.com>
2019-12-03 19:28:51 +0800

    函数

--- DWDatabase/DWDatabaseFunction.h
+++ DWDatabase/DWDatabaseFunction.h
@@ -29,12 +29,14 @@ BOOL supportSavingWithPropertyInfo(DWPrefix_YYClassPropertyInfo * property);
 ///时间转换格式化
 NSDateFormatter *dateFormatter(void);
 
-///
+///根据对应的属性类型转换值
 id transformValueWithPropertyInfo(id value,DWPrefix_YYClassPropertyInfo * property);
 
+///根据对应的对象类型转化值
 id transformValueWithType(id value,DWPrefix_YYEncodingType encodingType,DWPrefix_YYEncodingNSType nsType);
 
 ///获取两个数组的交集
 NSArray * intersectionOfArray(NSArray * arr1,NSArray * arr2);
 
+///从数组1中减去数组2
 NSArray * minusArray(NSArray * arr1,NSArray * arr2);
