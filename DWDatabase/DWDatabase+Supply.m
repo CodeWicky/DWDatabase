637aaeefad5b2fa70a6c582fa617583a5f679683
Wicky <codewicky@163.com>
2021-11-26 16:47:28 +0800

    修复增加字段

--- DWDatabase/DWDatabase+Supply.m
+++ DWDatabase/DWDatabase+Supply.m
@@ -11,76 +11,6 @@
 #import "DWDatabaseFunction.h"
 #import "DWDatabase+Private.h"
 
-@interface DWMetaClassInfo (Supply)
-
-@property (nonatomic ,strong ,readonly) NSMutableSet * fieldsSuppliedSet;
-
-@property (nonatomic ,assign) BOOL hasSuppliedTblFields;
-
-@end
-
-@implementation DWMetaClassInfo (Supply)
-
-+(NSArray *)fieldsToSupplyForClass:(Class)cls withKeys:(NSArray <NSString *>*)keys {
-    if (cls == NULL) {
-        return nil;
-    }
-    if (keys.count == 0) {
-        return nil;
-    }
-    DWMetaClassInfo * classInfo = [self classInfoFromClass:cls];
-    NSMutableSet * keysToSupply = [NSMutableSet setWithArray:keys];
-    [keysToSupply minusSet:classInfo.fieldsSuppliedSet];
-    if (keysToSupply.count == 0) {
-        return nil;
-    }
-    return keysToSupply.allObjects;
-}
-
-+(void)supplyFieldsForClass:(Class)cls withKeys:(NSArray <NSString *>*)keys {
-    NSArray * keysToSupply = [self fieldsToSupplyForClass:cls withKeys:keys];
-    if (!keysToSupply.count) {
-        return;
-    }
-    DWMetaClassInfo * classInfo = [self classInfoFromClass:cls];
-    [classInfo.fieldsSuppliedSet addObjectsFromArray:keysToSupply];
-}
-
-+(void)supplyTblFieldsForClass:(Class)cls withKeys:(NSArray <NSString *>*)keys {
-    if (cls == NULL) {
-        return;
-    }
-    if (keys.count == 0) {
-        return;
-    }
-    DWMetaClassInfo * classInfo = [self classInfoFromClass:cls];
-    if (classInfo.hasSuppliedTblFields) {
-        return;
-    }
-    [self supplyFieldsForClass:cls withKeys:keys];
-    classInfo.hasSuppliedTblFields = YES;
-}
-
-#pragma mark --- setter/getter ---
--(NSMutableSet *)fieldsSuppliedSet {
-    NSMutableSet * set = objc_getAssociatedObject(self, _cmd);
-    if (!set) {
-        set = [NSMutableSet set];
-        objc_setAssociatedObject(self, _cmd, set, OBJC_ASSOCIATION_RETAIN);
-    }
-    return set;
-}
-
--(BOOL)hasSuppliedTblFields {
-    return [objc_getAssociatedObject(self, _cmd) boolValue];
-}
-
--(void)setHasSuppliedTblFields:(BOOL)hasSuppliedTblFields {
-    objc_setAssociatedObject(self, @selector(hasSuppliedTblFields), @(hasSuppliedTblFields), OBJC_ASSOCIATION_ASSIGN);
-}
-
-@end
-
 @implementation DWDatabase (Supply)
 
 -(DWDatabaseResult *)_entry_supplyFieldIfNeededWithClass:(Class)cls configuration:(DWDatabaseConfiguration *)conf {
@@ -96,32 +26,24 @@
     }
     
     NSArray * allKeysInTbl = [self queryAllFieldInTable:YES class:cls configuration:conf].result;
-    [DWMetaClassInfo supplyTblFieldsForClass:cls withKeys:allKeysInTbl];
-    NSArray * saveProArray = [DWMetaClassInfo fieldsToSupplyForClass:cls withKeys:keys];
-    if (saveProArray.count == 0) {
+    NSMutableSet * keysToSupply = [NSMutableSet setWithArray:keys];
+    [keysToSupply minusSet:[NSSet setWithArray:allKeysInTbl]];
+    if (keysToSupply.count == 0) {
         return [DWDatabaseResult successResultWithResult:nil];
     }
     
+    NSArray * saveProArray = keysToSupply.allObjects;
     NSDictionary * map = databaseMapFromClass(cls);
     NSDictionary * defaultValueMap = databaseFieldDefaultValueMapFromClass(cls);
     NSDictionary <NSString *,DWPrefix_YYClassPropertyInfo *>* propertys = [DWDatabase propertyInfosWithClass:cls keys:saveProArray];
-    NSMutableArray * successKeys = [NSMutableArray arrayWithCapacity:propertys.count];
     [propertys enumerateKeysAndObjectsUsingBlock:^(NSString * _Nonnull key, DWPrefix_YYClassPropertyInfo * _Nonnull obj, BOOL * _Nonnull stop) {
         ///转化完成的键名及数据类型
         result = [self dw_addFieldsToTableForClass:cls withKey:key propertyInfo:obj dbMap:map defaultValueMap:defaultValueMap configuration:conf];
         
         if (!result.success) {
             *stop = YES;
-        } else {
-            if (obj.name.length) {
-                [successKeys addObject:obj.name];
-            }
         }
     }];
-    
-    if (successKeys.count) {
-        [DWMetaClassInfo supplyFieldsForClass:cls withKeys:successKeys];
-    }
     
     return result;
 }
